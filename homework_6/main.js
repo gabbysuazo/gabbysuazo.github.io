543c8c9f0d650443889d72cc1bce85e85a048bf1
Gabriela Suazo <gsuazo@andrew.cmu.edu>
2020-10-26 23:18:32 -0400

    Test radio buttons in place of buttons on product details page

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,1 +1,14 @@
+// Choose Glazing Option
 
+let x = 0
+
+function chooseGlazing() {
+    document.getElementById("glazing-option").selected = "false";
+    let glazingOption = event.target;
+    glazingOption.selected = "true";
+
+    if (glazingOption.selected = "true") {
+        glazingOption.style.backgroundColor = "#6A8DA5";
+        glazingOption.style.color = "#f8f8f8";
+    }
+}

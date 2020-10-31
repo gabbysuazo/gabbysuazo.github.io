4177accfff25d74db67620f4fe2c53df62775ead
Gabriela Suazo <gsuazo@andrew.cmu.edu>
2020-10-30 21:36:17 -0400

    Style radio buttons on selection

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,14 +1,11 @@
-// Choose Glazing Option
+// productArr = []
 
-let x = 0
+// class Product {
+//     constructor(bunFlavor, glazing, quantity, price) {
+//         this.bunFlavor = bunFlavor
+//         this.glazing = glazing
+//         this.quantity = quantity
+//         this.price = price
+//     }
+// }
 
-function chooseGlazing() {
-    document.getElementById("glazing-option").selected = "false";
-    let glazingOption = event.target;
-    glazingOption.selected = "true";
-
-    if (glazingOption.selected = "true") {
-        glazingOption.style.backgroundColor = "#6A8DA5";
-        glazingOption.style.color = "#f8f8f8";
-    }
-}

6eb4892d0c1f68b59ac44a5dca9c04726d0bbf69
Gabriela Suazo <gsuazo@andrew.cmu.edu>
2020-10-30 22:04:22 -0400

    Add functionaly so details page adds an image caption on glazing selection

--- homework_6/main.js
+++ homework_6/main.js
@@ -9,3 +9,11 @@
 //     }
 // }
 
+document.getElementById("glazing-options").addEventListener("click", function() { 
+    var glazing = document.getElementsByName('glazing'); 
+      
+    for(i = 0; i < glazing.length; i++) { 
+        if(glazing[i].checked) 
+        document.getElementById("bun-caption").innerHTML = "glazing: "+glazing[i].value; 
+    } 
+});

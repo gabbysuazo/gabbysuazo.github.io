bd46d09c373dd897ab252466a0f6a789ac3469a2
Gabriela Suazo <gsuazo@andrew.cmu.edu>
2020-11-09 21:01:21 -0600

    Add cart functionality so products and item totals display in cart

--- homework_6/main.js
+++ homework_6/main.js
@@ -54,55 +54,136 @@ document.getElementById("add-cart-button").addEventListener("click", function() 
 
 // Add to Cart
 
-let carts = document.querySelectorAll('#add-cart-button');
+productArr = []
 
-let products = [
-    {
-        name: 'Original',
-        tag: 'original',
-        price: 2.49,
-        inCart: 0
-    },
-    {
-        name: 'Blackberry',
-        tag: 'blackberry',
-        price: 2.49,
-        inCart: 0
-    },
-    {
-        name: 'Walnut',
-        tag: 'walnut',
-        price: 2.49,
-        inCart: 0
-    },
-]
-
-
-for (let i=0; i < carts.length; i++) {
-    carts[i].addEventListener('click', () => {
-        cartNumbers();
-        location.reload();
-    })
-}
-
-function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
-
-    if (productNumbers) {
-        document.querySelector('#cart-item-amount').textContent = productNumbers;
+class Product {
+    constructor (flavor, glazing, quantity, price, photo) {
+        this.flavor = flavor;
+        this.glazing = glazing;
+        this.quantity = quantity;
+        this.price = price;
+        this.photo = photo;
     }
 }
 
-function cartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
+function checkoutPageLoaded() {
+    let loadedProductArr = localStorage.getItem('order');
+    let productArr2 = JSON.parse(loadedProductArr);
 
-    productNumbers = parseInt(productNumbers);
+    for (let i=0; i < productArr2.length; i++) {
+        let div1 = document.createElement("DIV");
+        div1.classList.add('item');
+        document.getElementById('items-left').appendChild(div1);
 
-    if (productNumbers) {
-        localStorage.setItem('cartNumbers', productNumbers + cartItemCountInt)
-    } else {
-        localStorage.setItem('cartNumbers', cartItemCountInt);
+        let div2 = document.createElement('DIV');
+        div2.classList.add('cart-quantity');
+        div1.appendChild(div2);
+
+        let quantity = document.createElement('H3');
+        quantity.innerHTML = productArr2[i].quantity;
+        div2.appendChild(quantity);
+
+        let div3 = document.createElement('DIV');
+        div3.classList.add('cart-item-name-container');
+        div1.appendChild(div3);
+
+        let img = document.createElement('IMG');
+        div3.appendChild(img);
+
+        if (productArr2[i].flavor == 'original') {
+            img.src = "images/detail-bun1.png";
+        }
+        if (productArr2[i].flavor == 'blackberry'){
+            img.src = "images/detail-bun2.png";
+        } 
+        if (productArr2[i].flavor == 'walnut') {
+            img.src = "images/detail-bun3.png";
+        } 
+        if (productArr2[i].flavor == 'original (gluten-free)') {
+            img.src = "images/detail-bun4.png";
+        } 
+        if (productArr2[i].flavor == 'pumpkin spice') {
+            img.src = "images/detail-bun5.png";
+        } 
+        if (productArr2[i].flavor == 'caramel pecan') {
+            img.src = "images/detail-bun6.png";
+        }
+
+        let div4 = document.createElement('DIV');
+        div4.classList.add('cart-item-name');
+        div3.appendChild(div4);
+
+        let name = document.createElement('H3');
+        name.innerHTML = productArr2[i].flavor;
+        div4.appendChild(name);
+
+        let glazing = document.createElement('H5');
+        glazing.innerHTML = 'glazing: ' + productArr2[i].glazing;
+        div4.appendChild(glazing);
+
+        let div5 = document.createElement('DIV');
+        div5.classList.add('item-price');
+        div1.appendChild(div5);
+
+        let itemTotalCost = (parseInt(productArr2[i].quantity) * parseFloat(productArr2[i].price)).toFixed(2);
+
+        let price = document.createElement('H3');
+        price.innerHTML = '$' + itemTotalCost;
+        div5.appendChild(price);
     }
+
+    let subtotal = 0;
+    let tax = 0.06;
+
+    for (var i=0; i < productArr2.length; i++) {
+        subtotal += parseFloat(productArr2[i].price * productArr2[i].quantity);
+    }
+
+    subtotal = subtotal.toFixed(2);
+
+    tax = subtotal * tax;
+    tax = tax.toFixed(2);
+
+    subtotal = parseFloat(subtotal);
+    tax = parseFloat(tax);
+
+    let total = subtotal + tax;
+    total = total.toFixed(2);
+
+    console.log(subtotal);
+    console.log(tax);
+    console.log(total);
+
+    document.getElementById('subtotal').innerHTML = subtotal;
+    document.getElementById('tax').innerHTML = tax;
+    document.getElementById('total').innerHTML = total;
+
+    document.getElementById('cart-item-amount').innerHTML = productArr2.length;
 }
 
-onLoadCartNumbers()
+function addToCart() {
+    let flavor = document.getElementById('product-name').innerHTML;
+    let glazing = document.getElementById('bun-caption').innerHTML;
+    let quantity = document.getElementById('quantity').innerHTML;
+    let price = document.getElementById('bun-price').innerHTML;
+    let photo = document.getElementById('product-image');
+
+    let bun = new Product (flavor, glazing, quantity, price, photo);
+
+    productArr.push(bun);
+
+    console.log(productArr);
+}
+
+function goToCheckoutPage() {
+    localStorage.setItem('order', JSON.stringify(productArr));
+
+    let loadedProductArr = localStorage.getItem('order');
+    let productArr2 = JSON.parse(loadedProductArr);
+}
+
+function cartCount() {
+    let loadedProductArr = localStorage.getItem('order');
+    let productArr2 = JSON.parse(loadedProductArr);
+    document.getElementById('cart-item-amount').innerHTML = productArr2.length;
+}

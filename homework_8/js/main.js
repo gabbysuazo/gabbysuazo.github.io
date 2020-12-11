c358c2b42b83e96b3e2820b07f080dd40f1885b3
Gabriela Suazo <gsuazo@andrew.cmu.edu>
2020-12-10 20:48:00 -0500

    Create seperate page website slashscreen

--- homework_8/js/main.js
+++ homework_8/js/main.js
@@ -12,7 +12,6 @@ $(function() {
             $("#footer-logo").attr("src", "images/lusid-logo-black.svg");
             $("#footer-ig").addClass("black-fill");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
            $(".header").removeClass("active");
            $(".g").removeClass("black-stroke");
            $(".line").removeClass("black-stroke");
@@ -25,6 +24,7 @@ $(function() {
         }
     });
 
+    // Menu Nav Mobile
     $(document).on("click", function() {
         if ($('#menu-checkbox').prop('checked') == true) {
 
@@ -54,6 +54,7 @@ $(function() {
         }
     });
 
+    // Menu Nav Desktop
     $(document).on("click", function() {
         if ($('#menu-checkbox').prop('checked') == true) {
             $('#desktop-menu').css({
@@ -76,6 +77,7 @@ $(function() {
         }
     });
 
+    // Close Menu Nav
     $("#close-menu").on("click", function() {
         $('#menu-checkbox').prop('checked', false);
     });
@@ -88,7 +90,7 @@ $(function() {
         event.stopPropagation();
     });
 
-
+    // Fixed Product Option Buttons
     $(window).on("scroll", function() {
         if($(window).scrollTop() > 200) {
             $(".product-buttons").removeClass("fixed-product-buttons");
@@ -96,87 +98,96 @@ $(function() {
             $(".product-buttons").addClass("fixed-product-buttons");
         }
     });
-    
+
+    // Splashscreen Auto Slideshow
+    $(document).ready(function() { 
+        setInterval(function(){ 
+            $('.slidenav__item--next').trigger('click'); 
+        }, 3000); 
+    });
+
+    $('#enter-button').on("click", function() {
+        
+    });
 });
 
 
 // Size Dropdown
-
 var x, i, j, l, ll, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
 x = document.getElementsByClassName("custom-select");
 l = x.length;
+
 for (i = 0; i < l; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
   ll = selElmnt.length;
-  /*for each element, create a new DIV that will act as the selected item:*/
   a = document.createElement("DIV");
+
   a.setAttribute("class", "select-selected");
   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
   x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
+
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
+
   for (j = 1; j < ll; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
     c.addEventListener("click", function(e) {
-        /*when an item is clicked, update the original select box,
-        and the selected item:*/
+
         var y, i, k, s, h, sl, yl;
         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
         sl = s.length;
         h = this.parentNode.previousSibling;
+
         for (i = 0; i < sl; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            yl = y.length;
-            for (k = 0; k < yl; k++) {
-              y[k].removeAttribute("class");
+            if (s.options[i].innerHTML == this.innerHTML) {
+                s.selectedIndex = i;
+                h.innerHTML = this.innerHTML;
+                y = this.parentNode.getElementsByClassName("same-as-selected");
+                yl = y.length;
+
+                for (k = 0; k < yl; k++) {
+                y[k].removeAttribute("class");
+                }
+
+                this.setAttribute("class", "same-as-selected");
+                break;
             }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
         }
         h.click();
     });
     b.appendChild(c);
   }
+
   x[i].appendChild(b);
+
   a.addEventListener("click", function(e) {
-      /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
       e.stopPropagation();
       closeAllSelect(this);
       this.nextSibling.classList.toggle("select-hide");
       this.classList.toggle("select-arrow-active");
     });
 }
+
+
 function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, xl, yl, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  xl = x.length;
-  yl = y.length;
-  for (i = 0; i < yl; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
     }
-  }
-  for (i = 0; i < xl; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
+
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
     }
-  }
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
